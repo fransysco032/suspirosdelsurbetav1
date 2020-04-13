b0619738154d2e1f843dd7c7f11aaa4d1072e347
Pedro Flores <pedrojfloresch@gmail.com>
2020-04-13 02:37:44 -0400

    suspirosdelsurv1.0

---
+++ application/views/principal/common/footer.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es"><head>
+	
+ 
+
+ 
+
+    
+ <!-- Vendor JS Files -->
+  <script src="<?=base_url();?>assets/vendor/jquery/jquery.min.js"></script>
+  <script src="<?=base_url();?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?=base_url();?>assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="<?=base_url();?>assets/vendor/php-email-form/validate.js"></script>
+  <script src="<?=base_url();?>assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="<?=base_url();?>assets/vendor/venobox/venobox.min.js"></script>
+  <script src="<?=base_url();?>assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="<?=base_url();?>assets/vendor/TweenMax/TweenMax.min.js"></script>
+  <script src="<?=base_url();?>assets/vendor/wavify/wavify.js"></script>
+  <script src="<?=base_url();?>assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="<?=base_url();?>assets/js/main.js"></script>
+
+
+
+
+
+
+
+
+<script src="<?=base_url();?>assets/jss/custom.js"></script>
+	<script src="<?=base_url();?>assets/plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+	
+	
+	
+	
+	</head>
+	<body>
---
+++ application/views/principal/common/head.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es"><head>
+		
+   
+
+	  <!-- Favicons -->
+  <link href="<?=base_url();?>assets/img/favicon.png" rel="icon">
+  <link href="<?=base_url();?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="<?=base_url();?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?=base_url();?>assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="<?=base_url();?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?=base_url();?>assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="<?=base_url();?>assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="<?=base_url();?>assets/vendor/line-awesome/css/line-awesome.min.css" rel="stylesheet">
+  <link href="<?=base_url();?>assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="<?=base_url();?>assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="<?=base_url();?>assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="<?=base_url();?>assets/css/style.css" rel="stylesheet">
+	
+	
+	
+	
+	
+
+
+
+
+
+	<link rel="stylesheet" type="text/css" href="<?=base_url();?>assets/plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+	<link rel="stylesheet" type="text/css" href="<?=base_url();?>assets/styles/main_styles.css">
+	
+
+
+
+    
+
+
+	
+	
+	
+	
+	</head>
+	<body>
